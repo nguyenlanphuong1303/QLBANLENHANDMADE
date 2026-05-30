032cad8974f813ad900fe5888f977bcba321ca82
Nguyen Lan Phuong <lanphuong1303@gmail.com>
2026-05-30 23:27:51 +0700

    update

--- app/views/shares/header.php
+++ app/views/shares/header.php
@@ -49,7 +49,7 @@ $wishlistItems = $_SESSION['wishlist_items'] ?? [];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SHOP GÌ CŨNG MÓC - ĐỘC ĐÁO - TỰ TAY - ĐẸP MẮT</title>
+    <title>GÌ CŨNG MÓC</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- FontAwesome CSS -->
